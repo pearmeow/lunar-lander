c9126ea84fd31ede28a892e5b62f9536c067ebe9
Perry Huang <perryhuang2015@gmail.com>
2025-10-16 23:54:47 -0400

    feat: add blocks

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 
 #include <raylib.h>
 
+#include <cstring>
+
 #include "CS3113/Rocket.h"
 
 // Global Constants
@@ -18,12 +20,18 @@ constexpr int SCREEN_WIDTH = 1600, SCREEN_HEIGHT = 900, FPS = 60, SIDES = 4;
 constexpr Vector2 ORIGIN = {SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0};
 
 constexpr char ROCKET_FP[] = "assets/rocket.png";
+constexpr char BLOCK_FP[] = "assets/block.png";
 
 // Global Variables
 AppStatus gAppStatus = RUNNING;
 Rocket* gRocket = nullptr;
 Vector2 gRocketScale = {50.0f, 50.0f};
 float gPreviousTicks = 0.0f;
+Vector2 gBlockScale = {100.0f, 100.0f};
+Entity* gBlocks = nullptr;
+
+// allocate enough space to put win/lose messages in later
+char gGameOver[10] = "";
 
 // Function Declarations
 void initialise();
@@ -37,6 +45,12 @@ bool isColliding(const Vector2* positionA, const Vector2* scaleA, const Vector2*
 void initialise() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lunar Lander");
     gRocket = new Rocket(ORIGIN, gRocketScale, ROCKET_FP);
+    gBlocks = new Entity[5];
+    for (size_t i = 0; i < 5; ++i) {
+        gBlocks[i].setTexture(BLOCK_FP);
+        gBlocks[i].setPosition({ORIGIN.x + i * gBlockScale.x, SCREEN_HEIGHT - gBlockScale.y});
+        gBlocks[i].setScale(gBlockScale);
+    }
     gRocket->displayCollider();
     SetTargetFPS(FPS);
 }
@@ -57,18 +71,30 @@ void processInput() {
 }
 
 void update() {
-    // if the game is over, return early because there are no more calculations
     float ticks = (float)GetTime();
     float deltaTime = ticks - gPreviousTicks;
     gPreviousTicks = ticks;
 
     // replace nullptr with terrain later
-    gRocket->update(deltaTime, nullptr, 0);
+    gRocket->update(deltaTime, gBlocks, 5);
+    // if the rocket reaches a lose condition
+    if (gRocket->getFlying()) {
+        // some safe string copying
+        strncpy(gGameOver, "You lose!", 9);
+    } else {
+        // if the rocket reaches a win condition
+        // some more safe string copying
+        strncpy(gGameOver, "You win!", 8);
+    }
 }
 
 void render() {
     BeginDrawing();
     gRocket->render();
+    for (size_t i = 0; i < 5; ++i) {
+        gBlocks[i].render();
+    }
+    // DrawText(gGameOver, ORIGIN.x, 0.0f, 30, WHITE);
     ClearBackground(BLACK);
     EndDrawing();
 }
